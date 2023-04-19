a1853c51fe46a241ad062e79405258fa34275fa0
Maciej Winnik <maciekwin3@gmail.com>
2023-04-19 15:02:28 +0200

    Fixed usage of NumberExpression and Lexer cleanup

--- Compiler/CodeAnalysis/Expressions/LiteralExpressionSyntax.cs
+++ Compiler/CodeAnalysis/Expressions/LiteralExpressionSyntax.cs
@@ -7,7 +7,7 @@
             LiteralToken = literalToken;
         }
 
-        public override SyntaxKind Kind => SyntaxKind.NumberExpression;
+        public override SyntaxKind Kind => SyntaxKind.LiteralExpression;
         public SyntaxToken LiteralToken { get; }
 
         public override IEnumerable<SyntaxNode> GetChildren()
