2996c878c1cd1a666a8ae086e450010ba42599e5
Maciej Winnik <maciekwin3@gmail.com>
2023-12-13 23:50:47 +0100

    Fix error messages

--- src/Compiler/CodeAnalysis/DiagnosticBag.cs
+++ src/Compiler/CodeAnalysis/DiagnosticBag.cs
@@ -44,19 +44,19 @@ namespace Compiler.CodeAnalysis
 
         public void ReportUndefinedUnaryOperator(TextSpan span, string operatorText, Type operandType)
         {
-            var message = $"Unary operator '{operatorText}' is not defined for type {operandType}.";
+            var message = $"Unary operator '{operatorText}' is not defined for type '{operandType}'.";
             Report(span, message);
         }
 
         public void ReportUndefinedBinaryOperator(TextSpan span, string operatorText, Type leftType, Type rightType)
         {
-            var message = $"Binary operator '{operatorText}' is not defined for types {leftType} and {rightType}.";
+            var message = $"Binary operator '{operatorText}' is not defined for types '{leftType}' and '{rightType}'.";
             Report(span, message);
         }
 
         public void ReportUndefinedName(TextSpan span, string name)
         {
-            var message = $"Varaible '{name}' doesn't exist.";
+            var message = $"Variable '{name}' doesn't exist.";
             Report(span, message);
         }
 
