d34ea45a88ec0fbda024bea5dfb3d886f8dfb075
Maciej Winnik <maciekwin3@gmail.com>
2023-12-24 12:26:49 +0100

    Check whether the last token is missing as opposed to a looking for arbitrary syntax errors

--- src/Repl/MoonwalkRepl.cs
+++ src/Repl/MoonwalkRepl.cs
@@ -66,12 +66,21 @@ namespace Repl
 
             var syntaxTree = SyntaxTree.Parse(text);
 
-            if (syntaxTree.Diagnostics.Any())
+            if (GetLastToken(syntaxTree.Root.Statement).IsMissing)
             {
                 return false;
             }
 
             return true;
+        }
+
+        private static SyntaxToken GetLastToken(SyntaxNode node)
+        {
+            if (node is SyntaxToken token)
+                return token;
+
+            // A syntax node should always contain at least 1 token.
+            return GetLastToken(node.GetChildren().Last());
         }
 
         protected override void EvaluateSubmission(string text)
