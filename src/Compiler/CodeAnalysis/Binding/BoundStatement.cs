e63dc4e7e0cb0a49690146829a2dae83512d1c41
Maciej Winnik <maciekwin3@gmail.com>
2023-12-24 11:34:02 +0100

    Moved BoundVariableDeclaration to separate file

--- src/Compiler/CodeAnalysis/Binding/BoundStatement.cs
+++ src/Compiler/CodeAnalysis/Binding/BoundStatement.cs
@@ -3,17 +3,4 @@
     internal abstract class BoundStatement : BoundNode
     {
     }
-
-    internal sealed class BoundVariableDeclaration : BoundStatement
-    {
-        public BoundVariableDeclaration(VariableSymbol variable, BoundExpression initializer)
-        {
-            Variable = variable;
-            Initializer = initializer;
-        }
-
-        public override BoundNodeKind Kind => BoundNodeKind.VariableDeclaration;
-        public VariableSymbol Variable { get; }
-        public BoundExpression Initializer { get; }
-    }
 }
