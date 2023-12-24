a3042f57a9d77822a33bdcd4bd3059b61c0c1169
Maciej Winnik <maciekwin3@gmail.com>
2023-12-24 11:30:37 +0100

    Remove double negation in BoundConditionalGotoStatement usage

--- src/Compiler/CodeAnalysis/Evaluator.cs
+++ src/Compiler/CodeAnalysis/Evaluator.cs
@@ -46,8 +46,7 @@ namespace Compiler.CodeAnalysis
                     case BoundNodeKind.ConditionalGotoStatement:
                         var cgs = (BoundConditionalGotoStatement)s;
                         var condition = (bool)EvaluateExpression(cgs.Condition);
-                        if (condition && !cgs.JumpIfFalse ||
-                            !condition && cgs.JumpIfFalse)
+                        if (condition && cgs.JumpIfTrue)
                         {
 
                             index = labelToIndex[cgs.Label];
--- src/Compiler/CodeAnalysis/Lowering/Lowerer.cs
+++ src/Compiler/CodeAnalysis/Lowering/Lowerer.cs
@@ -62,7 +62,7 @@ namespace Compiler.CodeAnalysis.Lowering
                 // <then>  
                 // end:
                 var endLabel = GenerateLabel();
-                var gotoFalse = new BoundConditionalGotoStatement(endLabel, node.Condition, true);
+                var gotoFalse = new BoundConditionalGotoStatement(endLabel, node.Condition, false);
                 var endLabelStatement = new BoundLabelStatement(endLabel);
                 var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(gotoFalse, node.ThenStatement, endLabelStatement));
                 return RewriteStatement(result);
@@ -86,7 +86,7 @@ namespace Compiler.CodeAnalysis.Lowering
                 var elseLabel = GenerateLabel();
                 var endLabel = GenerateLabel();
 
-                var gotoFalse = new BoundConditionalGotoStatement(elseLabel, node.Condition, true);
+                var gotoFalse = new BoundConditionalGotoStatement(elseLabel, node.Condition, false);
                 var gotoEndStatement = new BoundGotoStatement(endLabel);
                 var elseLabelStatement = new BoundLabelStatement(elseLabel);
                 var endLabelStatement = new BoundLabelStatement(endLabel);
@@ -124,7 +124,7 @@ namespace Compiler.CodeAnalysis.Lowering
             var gotoCheck = new BoundGotoStatement(checkLabel);
             var continueLabelStatement = new BoundLabelStatement(continueLabel);
             var checkLabelStatement = new BoundLabelStatement(checkLabel);
-            var gotoTrue = new BoundConditionalGotoStatement(continueLabel, node.Condition, false);
+            var gotoTrue = new BoundConditionalGotoStatement(continueLabel, node.Condition);
             var endLabelStatement = new BoundLabelStatement(endLabel);
 
             var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(
