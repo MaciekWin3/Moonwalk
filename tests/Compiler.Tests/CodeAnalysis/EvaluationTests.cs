27a149ab8251282ba6c94b03c1fadd00eb28ce70
Maciej Winnik <maciekwin3@gmail.com>
2024-05-25 19:06:56 +0200

    Prevent infinite loops when parsing invalid

--- tests/Compiler.Tests/CodeAnalysis/EvaluationTests.cs
+++ tests/Compiler.Tests/CodeAnalysis/EvaluationTests.cs
@@ -116,6 +116,43 @@ namespace Compiler.Tests.CodeAnalysis
         }
 
         [Test]
+        public void Evaluator_InvokeFunctionArguments_NoInfiniteLoop()
+        {
+            var text = @"
+                print(""Hi""[[=]][)]
+            ";
+
+            var diagnostics = @"
+                Unexpected token <EqualsToken>, expected <CloseParenthesisToken>.
+                Unexpected token <EqualsToken>, expected <IdentifierToken>.
+                Unexpected token <CloseParenthesisToken>, expected <IdentifierToken>.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Test]
+        public void Evaluator_FunctionParameters_NoInfiniteLoop()
+        {
+            var text = @"
+                function hi(name: string[[[=]]][)]
+                {
+                    print(""Hi "" + name + ""!"" )
+                }[]
+            ";
+
+            var diagnostics = @"
+                Unexpected token <EqualsToken>, expected <CloseParenthesisToken>.
+                Unexpected token <EqualsToken>, expected <OpenBraceToken>.
+                Unexpected token <EqualsToken>, expected <IdentifierToken>.
+                Unexpected token <CloseParenthesisToken>, expected <IdentifierToken>.
+                Unexpected token <EndOfFileToken>, expected <CloseBraceToken>.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Test]
         public void EvaluatorIfStatementReportsCannotConvert()
         {
             // Arrange
