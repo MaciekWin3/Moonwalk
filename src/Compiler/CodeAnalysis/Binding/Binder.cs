a81ec6a51bdd7f707457206d165d3eab4ca38507
Maciej Winnik <maciekwin3@gmail.com>
2024-07-15 15:55:23 +0200

    Minor fixes to binding of call arguments

--- src/Compiler/CodeAnalysis/Binding/Binder.cs
+++ src/Compiler/CodeAnalysis/Binding/Binder.cs
@@ -439,22 +439,48 @@ namespace Compiler.CodeAnalysis.Binding
 
             if (syntax.Arguments.Count != function.Parameters.Length)
             {
-                diagnostics.ReportWrongArgumentCount(syntax.Span, function.Name, function.Parameters.Length, syntax.Arguments.Count);
+                TextSpan span;
+                if (syntax.Arguments.Count > function.Parameters.Length)
+                {
+                    SyntaxNode firstExceedingNode;
+                    if (function.Parameters.Length > 0)
+                    {
+                        firstExceedingNode = syntax.Arguments.GetSeparator(function.Parameters.Length - 1);
+                    }
+                    else
+                    {
+                        firstExceedingNode = syntax.Arguments[0];
+                    }
+                    var lastExceedingArgument = syntax.Arguments[^1];
+                    span = TextSpan.FromBounds(firstExceedingNode.Span.Start, lastExceedingArgument.Span.End);
+                }
+                else
+                {
+                    span = syntax.CloseParenthesisToken.Span;
+                }
+                diagnostics.ReportWrongArgumentCount(span, function.Name, function.Parameters.Length, syntax.Arguments.Count);
                 return new BoundErrorExpression();
             }
 
-            var arguments = ImmutableArray.CreateBuilder<BoundExpression>();
 
+            bool hasErrors = false;
             for (int i = 0; i < syntax.Arguments.Count; i++)
             {
                 var argument = boundArguments[i];
                 var parameter = function.Parameters[i];
+
                 if (argument.Type != parameter.Type)
                 {
-                    diagnostics.ReportWrongArgumentType(syntax.Arguments[i].Span, parameter.Name, parameter.Type, argument.Type);
-                    return new BoundErrorExpression();
+                    if (argument.Type != TypeSymbol.Error)
+                    {
+                        diagnostics.ReportWrongArgumentType(syntax.Arguments[i].Span, parameter.Name, parameter.Type, argument.Type);
+                    }
+                    hasErrors = true;
                 }
-                arguments.Add(argument);
+            }
+            if (hasErrors)
+            {
+                return new BoundErrorExpression();
             }
 
             return new BoundCallExpression(function, boundArguments.ToImmutable());
--- tests/Compiler.Tests/CodeAnalysis/EvaluationTests.cs
+++ tests/Compiler.Tests/CodeAnalysis/EvaluationTests.cs
@@ -123,6 +123,34 @@ namespace Compiler.Tests.CodeAnalysis
         }
 
         [Test]
+        public void Evaluator_InvokeFunctionArguments_Missing()
+        {
+            var text = @"
+                print([)]
+            ";
+
+            var diagnostics = @"
+                Function 'print' requires 1 arguments but was given 0.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Test]
+        public void Evaluator_InvokeFunctionArguments_Exceeding()
+        {
+            var text = @"
+                print(""Hello""[, "" "", "" world!""])
+            ";
+
+            var diagnostics = @"
+                Function 'print' requires 1 arguments but was given 3.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Test]
         public void Evaluator_InvokeFunctionArguments_NoInfiniteLoop()
         {
             var text = @"
