c4adb73bf88ce16817b1ba84ab81b331c0d2b871
Maciej Winnik <maciekwin3@gmail.com>
2024-07-20 21:57:37 +0200

    Add support for multiple files

--- src/Compiler/Program.cs
+++ src/Compiler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Core.CodeAnalysis;
+using Core.CodeAnalysis.Symbols;
 using Core.CodeAnalysis.Syntax;
 using Core.IO;
 
@@ -6,32 +7,37 @@ namespace Compiler
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             if (args.Length == 0)
             {
-                Console.Error.WriteLine("Usage: mwc <source-paths>");
+                Console.Error.WriteLine("usage: mc <source-paths>");
                 return;
             }
 
-            if (args.Length > 1)
+            var paths = GetFilePaths(args);
+            var syntaxTrees = new List<SyntaxTree>();
+            var hasErrors = false;
+
+            foreach (var path in paths)
             {
-                Console.Error.WriteLine("Error: Only one file can be compiled at a time.");
-                return;
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine($"error: file '{path}' doesn't exist");
+                    hasErrors = true;
+                    continue;
+                }
+                var syntaxTree = SyntaxTree.Load(path);
+                syntaxTrees.Add(syntaxTree);
             }
 
-            var path = args.Single();
-
-            if (!File.Exists(path))
+            if (hasErrors)
             {
-                Console.WriteLine($"error: file '{path}' doesn't exist");
                 return;
             }
 
-            var syntaxTree = SyntaxTree.Load(path);
-
-            var compilation = new Compilation(syntaxTree);
-            var result = compilation.Evaluate([]);
+            var compilation = new Compilation(syntaxTrees.ToArray());
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
 
             if (!result.Diagnostics.Any())
             {
@@ -44,6 +50,25 @@ namespace Compiler
             {
                 Console.Error.WriteDiagnostics(result.Diagnostics);
             }
+        }
+
+        private static IEnumerable<string> GetFilePaths(IEnumerable<string> paths)
+        {
+            var result = new SortedSet<string>();
+
+            foreach (var path in paths)
+            {
+                if (Directory.Exists(path))
+                {
+                    result.UnionWith(Directory.EnumerateFiles(path, "*.ms", SearchOption.AllDirectories));
+                }
+                else
+                {
+                    result.Add(path);
+                }
+            }
+
+            return result;
         }
     }
 }
